e61bd4e2e8a5f9d4bdc92262f38a5a195ff98ce0
jgpawletko <jgpawletko@gmail.com>
2024-07-01 18:03:36 -0400

    WIP: snapshot om.go

---
+++ ead/om.go
@@ -0,0 +1,41 @@
+package ead
+
+import "encoding/xml"
+
+type IndexOption int64
+
+const (
+	Displayable IndexOption = iota
+	Facetable
+	Searchable
+	Sortable
+	StoredSortable
+)
+
+type Term struct {
+	Name    string
+	Path    string
+	IndexAs []IndexOption
+}
+
+type Terminology struct {
+	Root  string
+	Terms []Term
+}
+
+type Field struct {
+	XMLName xml.Name `xml:"field"`
+	Name    string   `xml:"name,attr"`
+	Value   string   `xml:",chardata"`
+}
+
+type SolrDoc struct {
+	Fields []Field `xml:"doc"`
+}
+
+var EADTerminology = Terminology{
+	Root: "ead",
+	Terms: []Term{
+		{"author", "filedesc/titlestmt/author", []IndexOption{Searchable, Displayable}},
+	},
+}
