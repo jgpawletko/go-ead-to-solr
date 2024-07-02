f40d39dcc37e764425dcc1a22e004d60642e7e22
jgpawletko <jgpawletko@gmail.com>
2024-07-02 09:43:08 -0400

    update ead_test.go
    Update order in TermTest scenarios
    Add SolrDoc generation test used for debugging

--- ead/ead_test.go
+++ ead/ead_test.go
@@ -1,6 +1,8 @@
 package ead
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
 
@@ -15,25 +17,20 @@ func TestGenFieldName(t *testing.T) {
 	t.Run("Test GenFieldName()", func(t *testing.T) {
 
 		scenarios := []TermTest{
-			{Term{"foo", String, "", []IndexOption{}}, StoredSearchable, "foo_tesim", "failed String/StoredSearchable"},
-			{Term{"bar", Text, "", []IndexOption{}}, StoredSearchable, "bar_tesim", "failed Text/StoredSearchable"},
-			{Term{"baz", Date, "", []IndexOption{}}, StoredSearchable, "baz_dtsim", "failed Date/StoredSearchable"},
-			{Term{"quux", Int, "", []IndexOption{}}, StoredSearchable, "quux_isim", "failed Int/StoredSearchable"},
-
-			{Term{"foo", String, "", []IndexOption{}}, Searchable, "foo_teim", "failed String/Searchable"},
-			{Term{"bar", Text, "", []IndexOption{}}, Searchable, "bar_teim", "failed Text/Searchable"},
-			{Term{"baz", Date, "", []IndexOption{}}, Searchable, "baz_dtim", "failed Date/Searchable"},
-			{Term{"quux", Int, "", []IndexOption{}}, Searchable, "quux_iim", "failed Int/Searchable"},
-
-			{Term{"foo", String, "", []IndexOption{}}, Dateable, "foo_dtsim", "failed String/Dateable"},
-			{Term{"bar", Text, "", []IndexOption{}}, Dateable, "bar_dtsim", "failed Text/Dateable"},
-			{Term{"baz", Date, "", []IndexOption{}}, Dateable, "baz_dtsim", "failed Date/Dateable"},
-			{Term{"quux", Int, "", []IndexOption{}}, Dateable, "quux_dtsim", "failed Int/Dateable"},
+			{Term{"foo", String, "", []IndexOption{}}, Displayable, "foo_ssm", "failed String/Displayable"},
+			{Term{"bar", Text, "", []IndexOption{}}, Displayable, "bar_ssm", "failed Text/Displayable"},
+			{Term{"baz", Date, "", []IndexOption{}}, Displayable, "baz_ssm", "failed Date/Displayable"},
+			{Term{"quux", Int, "", []IndexOption{}}, Displayable, "quux_ssm", "failed Int/Displayable"},
 
 			{Term{"foo", String, "", []IndexOption{}}, Facetable, "foo_sim", "failed String/Facetable"},
 			{Term{"bar", Text, "", []IndexOption{}}, Facetable, "bar_sim", "failed Text/Facetable"},
 			{Term{"baz", Date, "", []IndexOption{}}, Facetable, "baz_sim", "failed Date/Facetable"},
 			{Term{"quux", Int, "", []IndexOption{}}, Facetable, "quux_sim", "failed Int/Facetable"},
+
+			{Term{"foo", String, "", []IndexOption{}}, Searchable, "foo_teim", "failed String/Searchable"},
+			{Term{"bar", Text, "", []IndexOption{}}, Searchable, "bar_teim", "failed Text/Searchable"},
+			{Term{"baz", Date, "", []IndexOption{}}, Searchable, "baz_dtim", "failed Date/Searchable"},
+			{Term{"quux", Int, "", []IndexOption{}}, Searchable, "quux_iim", "failed Int/Searchable"},
 
 			{Term{"foo", String, "", []IndexOption{}}, Sortable, "foo_si", "failed String/Sortable"},
 			{Term{"bar", Text, "", []IndexOption{}}, Sortable, "bar_tei", "failed Text/Sortable"},
@@ -45,10 +42,15 @@ func TestGenFieldName(t *testing.T) {
 			{Term{"baz", Date, "", []IndexOption{}}, StoredSortable, "baz_dtsi", "failed Date/StoredSortable"},
 			{Term{"quux", Int, "", []IndexOption{}}, StoredSortable, "quux_isi", "failed Int/StoredSortable"},
 
-			{Term{"foo", String, "", []IndexOption{}}, Displayable, "foo_ssm", "failed String/Displayable"},
-			{Term{"bar", Text, "", []IndexOption{}}, Displayable, "bar_ssm", "failed Text/Displayable"},
-			{Term{"baz", Date, "", []IndexOption{}}, Displayable, "baz_ssm", "failed Date/Displayable"},
-			{Term{"quux", Int, "", []IndexOption{}}, Displayable, "quux_ssm", "failed Int/Displayable"},
+			{Term{"foo", String, "", []IndexOption{}}, StoredSearchable, "foo_tesim", "failed String/StoredSearchable"},
+			{Term{"bar", Text, "", []IndexOption{}}, StoredSearchable, "bar_tesim", "failed Text/StoredSearchable"},
+			{Term{"baz", Date, "", []IndexOption{}}, StoredSearchable, "baz_dtsim", "failed Date/StoredSearchable"},
+			{Term{"quux", Int, "", []IndexOption{}}, StoredSearchable, "quux_isim", "failed Int/StoredSearchable"},
+
+			{Term{"foo", String, "", []IndexOption{}}, Dateable, "foo_dtsim", "failed String/Dateable"},
+			{Term{"bar", Text, "", []IndexOption{}}, Dateable, "bar_dtsim", "failed Text/Dateable"},
+			{Term{"baz", Date, "", []IndexOption{}}, Dateable, "baz_dtsim", "failed Date/Dateable"},
+			{Term{"quux", Int, "", []IndexOption{}}, Dateable, "quux_dtsim", "failed Int/Dateable"},
 		}
 
 		for _, scenario := range scenarios {
@@ -57,5 +59,25 @@ func TestGenFieldName(t *testing.T) {
 				t.Errorf("unexpected result: %s: want: '%s', got: '%s'", scenario.Comment, scenario.Expected, got)
 			}
 		}
+	})
+}
+
+func TestGenSolrDoc(t *testing.T) {
+
+	t.Run("Test GenSolrDoc()", func(t *testing.T) {
+
+		EADXML, err := os.ReadFile("./testdata/input/fales/mss_104.xml")
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+
+		data, errors := GenSolrDoc(EADXML, EADTerminology)
+		if len(errors) != 0 {
+			for _, eMsg := range errors {
+				fmt.Printf("%s\n", eMsg)
+			}
+			t.Errorf("failed to generate Solr document")
+		}
+		fmt.Printf("%v\n", data)
 	})
 }
